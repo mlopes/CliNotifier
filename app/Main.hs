69654a4c30bb91c8a450a4e2e4480995dacb4135
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-07-27 19:42:31 +0100

    Process a fake command

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,14 @@
 module Main where
 
 import SlackService
+import Data.Time.Clock
 
 main :: IO ()
-main = sendMessage "https://hooks.slack.com/services/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"
+main = do
+  startTime <- getCurrentTime
+  let message = "ls -lha"
+  let messageDispatcher = sendMessage "https://hooks.slack.com/services/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"
+  endTime <- getCurrentTime
+  let elapsedTime = diffUTCTime endTime startTime
+  _ <- messageDispatcher startTime endTime elapsedTime message
+  return ()
--- src/SlackService.hs
+++ src/SlackService.hs
@@ -5,25 +5,31 @@ module SlackService
     , Hook
     ) where
 
+import Prelude hiding (concat)
 import Data.Text
 import Network.Linklater
 import Network.Linklater.Types
 import Control.Monad.Except
+import Data.Time.Clock
 
 type Hook = Text
 
-sendMessage :: Hook -> IO ()
-sendMessage hook = do
-  messageResult <- publishMessage $ Config hook
+sendMessage :: Hook -> UTCTime -> UTCTime -> NominalDiffTime -> Text -> IO ()
+sendMessage hook startTime endTime elapsedTime cliCommand = do
+  messageResult <- publishMessage (Config hook) (buildMessage startTime endTime elapsedTime cliCommand)
   case messageResult of
     Left e -> putStrLn ("An error occurred!" <> show e)
     Right _ -> return ()
 
-publishMessage :: Config -> IO (Either RequestError ())
-publishMessage config = runExceptT $ say message config
 
-message :: Message
-message = SimpleMessage (icon) "Some Bot" channel "Some more text"
+buildMessage :: UTCTime -> UTCTime -> NominalDiffTime -> Text -> Text
+buildMessage startTime endTime elapsedTime cliCommand = concat ["`", cliCommand, "` started at: ", pack $ show startTime, " finished at: ", pack $ show endTime, " ran for : ", pack $ show elapsedTime, "."]
+
+publishMessage :: Config -> Text -> IO (Either RequestError ())
+publishMessage config text = runExceptT $ say (message text) config
+
+message :: Text -> Message
+message = SimpleMessage (icon) "Some Bot" channel
 
 channel :: Channel
 channel = Channel "" ""
