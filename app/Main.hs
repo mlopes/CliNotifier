d4af2c3104f355770861207de36a510768ae8fc7
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-07-27 21:57:11 +0100

    Use command name for notification

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,25 @@
 
 module Main where
 
-import SlackService
 import Data.Time.Clock
+import Data.Text
+import System.Environment
+import SlackService
 
 main :: IO ()
 main = do
   startTime <- getCurrentTime
-  let message = "ls -lha"
+  args <- getArgs
+  case (fmap pack args) of
+    [] -> putStrLn("Missing command to notify on.")
+    cliCommand:cliCommandParameters -> processMessage cliCommand startTime
+  return ()
+
+processMessage :: Text -> UTCTime -> IO ()
+processMessage cliCommand startTime = do
   let messageDispatcher = sendMessage "https://hooks.slack.com/services/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"
   endTime <- getCurrentTime
   let elapsedTime = diffUTCTime endTime startTime
-  _ <- messageDispatcher $ NotificationMessage startTime endTime elapsedTime message
+  _ <- messageDispatcher $ NotificationMessage startTime endTime elapsedTime cliCommand
   return ()
+
