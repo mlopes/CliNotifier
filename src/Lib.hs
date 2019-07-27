4d29615290b3e8a75eb29c6e4559fee5c08ffdc1
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-07-27 17:10:24 +0100

    Publish a dummy message to a hard coded channel for now

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( slackResult
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Network.Linklater
+import Network.Linklater.Types
+import Control.Monad.Except
+
+slackResult :: IO ()
+slackResult = do
+  messageResult <- sendMessage
+  case messageResult of
+    Left e -> putStrLn ("An error occurred!" <> show e)
+    Right _ -> return ()
+
+sendMessage :: IO (Either RequestError ())
+sendMessage = runExceptT $ say message config
+
+config :: Config
+config = Config "https://hooks.slack.com/services/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"
+
+message :: Message
+message = SimpleMessage (icon) "Some Bot" channel "Some more text"
+
+channel :: Channel
+channel = Channel "" ""
+
+icon :: Icon
+icon = EmojiIcon "computer"
+
